de22a14f16d4b2a5e9568c3512a145310223aade
Zoe <zoeguadalupesantillan@gmail.com>
2025-01-22 14:54:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ function intentoDeBoton() {
   alert('El boton fue clickeado');
 }
 
-Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+//Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
