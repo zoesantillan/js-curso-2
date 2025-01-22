9931782b5512a2c4674681520e3895cccbfe8062
Zoe <zoeguadalupesantillan@gmail.com>
2025-01-22 14:53:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ titulo.innerHTML = 'Hora del Desafio';
 function intentoDeBoton() {
   alert('El boton fue clickeado');
 }
+
+Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
