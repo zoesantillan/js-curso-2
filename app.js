5a74ebf0ae41a3422eae279dac1bd6799cc8095b
Zoe <zoeguadalupesantillan@gmail.com>
2025-01-22 14:59:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ function intentoDeBoton() {
 }
 
 //Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+function intentoDePrompt() {
+  prompt('Escriba el nombre de una ciudad de Brasil');
+} while { 
+alert('Estuve en 
