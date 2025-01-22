72d5efbd483e68f7bf33e39d7741e26983be38ef
Zoe <zoeguadalupesantillan@gmail.com>
2025-01-22 15:14:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function intentoDeBoton() {
 
 //Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
 function intentoDePrompt() {
-  prompt('Escriba el nombre de una ciudad de Brasil');
-} while { 
-alert('Estuve en 
+  let ciudad = prompt("¿Cuál es el nombre de una ciudad de Brasil?");
+      
+        if (ciudad) {
+        alert("Estuve en " + ciudad + " y me acordé de ti");
+      }
+}
+
+//Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
