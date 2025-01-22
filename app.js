5cc2652d9881be05b48b2d16d3e6dacf81c1d84c
Zoe <zoeguadalupesantillan@gmail.com>
2025-01-22 14:47:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+//Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafio';
 
+//Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
+function intentoDeBoton() {
+  alert('El boton fue clickeado');
+}
